f87b89dc36fdfa63e0be3efefa3bdd45ab5a1171
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-08 02:07:11 -0800

    add putchar.c

---
+++ _putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes the character c to stout
+ * @c: The character to print
+ * Return: On succes 1, or -1 on error
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
