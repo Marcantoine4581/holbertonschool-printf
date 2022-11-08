79996a2870ef8e82e76f2e7e2113a9ba6f16b0fa
Marcantoine4581 <marcantoine.vannier@gmail.com>
2022-11-08 09:50:40 -0800

    bug fixed

--- _printf.c
+++ _printf.c
@@ -9,16 +9,16 @@
 int (*check_format(const char *format))(va_list)
 {
 	int i = 0;
-	s_format find_f[] = {
+	s_format type[] = {
 		{"c", print_char},
 		{"s", print_string},
 		{NULL, NULL}
 	};
 
-	while (find_f[i].sc)
+	while (type[i].sc)
 	{
-		if (find_f[i].sc[0] == (*format))
-			return (find_f[i].f);
+		if (type[i].sc[0] == (*format))
+			return (type[i].f);
 		i++;
 	}
 	return (NULL);
@@ -39,35 +39,33 @@ int _printf(const char *format, ...)
 		return (-1);
 
 	va_start(ap, format);
-	while (format && format[i])
+
+	while (format[i])
 	{
-		if (format[i] != '%')
+		while (format[i] != '%' && format[i])
 		{
 			_putchar(format[i]);
 			count_c++;
 			i++;
+		}
+		if (format[i] == '\0')
+			return (count_c);
+		f = check_format(&format[i + 1]);
+
+		if (f != NULL)
+		{
+			count_c += f(ap);
+			i += 2;
 			continue;
 		}
+		if (!format[i + 1])
+			return (-1);
+		_putchar(format[i]);
+		count_c++;
+		if (format[i + 1] == '%')
+			i += 2;
 		else
-		{
-			if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				count_c++;
-				i += 2;
-				continue;
-			}
-			else
-			{
-				f = check_format(&format[i + 1]);
-				if (f == NULL)
-					return (-1);
-				i += 2;
-				count_c += f(ap);
-				continue;
-			}
-		}
-		i++;
+			i++;
 	}
 	va_end(ap);
 	return (count_c);
