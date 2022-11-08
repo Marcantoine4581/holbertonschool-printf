ec8fe6fa2607da655e7d122ab8d52f6d2279459c
EXTRADEEN <dinumira@gmail.com>
2022-11-08 02:31:10 -0800

    added main.h

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct print - definition of structure
+ * @sc: The specifiers
+ * @f: function
+ */
+typedef struct s_format
+{
+	char *sc;
+	int (*f)(va_list arg);
+}s_format;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(va_list args);
+int print_string(va_list args);
+
+#endif /* _MAIN_H */
